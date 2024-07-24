9989af5cb8790aa97baddde3cb0d38a9e0377bbe
rashidiy <bubbless7456@gmail.com>
2024-07-24 13:05:16 +0500

    123

--- adds.py
+++ adds.py
@@ -2,18 +2,20 @@ import os.path
 import subprocess
 
 
-def create_service_file(service_name, file_path, device_id, security_key, host, port, description='Device', python_path='/usr/bin/python3'):
+def create_service_file(service_name, file_path, device_id, security_key, host, port, description='Device',
+                        python_path='/usr/bin/python3'):
     service_path = f'/etc/systemd/system/{service_name}.service'
     try:
         with open('template.txt') as template_file:
             template = template_file.read()
         with open(service_path, 'w') as service_file:
             service_file.write(template.format(
-                    description=description,
-                    python_path=python_path,
-                    file_path=os.path.abspath(file_path),
-                    environment=f'"DEVICE_ID={device_id}" "SECURITY_KEY={security_key}" "HOST={host}" "PORT={port}"',
-                ))
+                description=description,
+                python_path=python_path,
+                file_path=os.path.abspath(file_path),
+                environment=f'"DEVICE_ID={device_id}" "SECURITY_KEY={security_key}" "HOST={host}" "PORT={port}"',
+            ))
+        enable_service(service_name)
         start_service(service_name)
         print(f"Service file {service_path} created successfully.")
     except IOError as e:
@@ -26,6 +28,14 @@ def reload_daemon():
         print("Systemd daemon reloaded successfully.")
     except subprocess.CalledProcessError as e:
         print(f"Failed to reload systemd daemon: {e}")
+
+
+def enable_service(service_name):
+    try:
+        subprocess.run(['sudo', 'systemctl', 'enable', service_name], check=True)
+        print(f"Service {service_name} enabled successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to enable {service_name}: {e}")
 
 
 def start_service(service_name):
