813510b186f6f4323153ab757b0a0343b670b49b
rashidiy <bubbless7456@gmail.com>
2024-09-05 15:28:43 +0500

    Merge remote-tracking branch 'origin/master'

--- device_client.py
+++ device_client.py
@@ -12,7 +12,8 @@ from websockets.legacy import client
 class WServer:
     security_key = os.getenv('SECURITY_KEY')
     device_id = os.getenv('DEVICE_ID')
-    uri = f"wss://{os.getenv('HOST')}:{os.getenv('PORT', 443)}/device/{device_id}/?security_key={security_key}"
+    port = os.getenv('PORT', 443)
+    uri = f"{'wss' if port == 443 else 'ws'}://{os.getenv('HOST')}:{port}/device/{device_id}/?security_key={security_key}"
     websocket = client.WebSocketClientProtocol
 
     async def accepted(self):
@@ -20,7 +21,7 @@ class WServer:
         await self.websocket.send(message)
 
     async def send_request(self, data):
-        auth_credits = data.get('auth')
+        auth_credits = data.pop('auth')
         match auth_credits.get('type'):
             case 'digest':
                 auth = HTTPDigestAuth(auth_credits.get('username'), auth_credits.get('password'))
@@ -28,11 +29,17 @@ class WServer:
                 auth = None
         status_code = -1
         content = None
+        response_channel = None
+        if 'response_channel' in data:
+            response_channel = data.pop('response_channel')
         try:
-            res = requests.get(auth=auth, timeout=100, **data)
+            req = requests.Request(**data, auth=auth).prepare()
+            with requests.Session() as session:
+                res = session.send(req, timeout=100)
             status_code = res.status_code
             text = res.text
-            content = base64.b64encode(res.content).decode('utf-8') if 'cgi' in data.get('url') else None
+            content = base64.b64encode(res.content).decode('utf-8') if 'cgi' in data.get(
+                'url') or 'picture' in data.get('url') else None
         except requests.exceptions.Timeout:
             text = "The request timed out"
         except requests.exceptions.RequestException as e:
@@ -41,13 +48,13 @@ class WServer:
         return {
             'status_code': status_code,
             'text': text,
-            'response_channel': data.get('response_channel'),
+            'response_channel': response_channel,
             'content': content
         }
 
     async def receive(self, data: dict):
         print(data)
-        if data.get('type') == 'send_request':
+        if data.pop('type') == 'send_request':
             res = await self.send_request(data)
             await self.websocket.send(json.dumps(res))
 
